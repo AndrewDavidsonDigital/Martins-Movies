057035c8cfc91e50d40f923ef220682fca080aa0
Andrew Davidson <andrew.davidson.digital@gmail.com>
2025-08-24 14:30:57 +1000

    UI: include interfaces for the API endpoints missed in prev commit: d11dcc996041c4cca30d07d2c0b61f3d16fe32a0

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -2,20 +2,16 @@ export interface IBaseProps {
   className?: string;
 }
 
-interface IIMDB{
-  IMDB?: string;
-}
-
-export interface IMovie extends IIMDB {
+export interface IMovie {
   adult: boolean;
-  backdrop_path: string;
+  backdrop_path: string | null;
   genre_ids: number[];
   id: number;
   original_language: string;
   original_title: string;
   overview: string;
   popularity: number;
-  poster_path: string;
+  poster_path: string | null;
   release_date: string;
   title: string;
   video: boolean;
@@ -29,7 +25,7 @@ export interface IDiscoverMoviesAPI {
   total_pages: number;
   total_results: number;
 }
-
+/*
 export interface IMovieExternalAPI {
   id: number;
   imdb_id: string;
@@ -37,4 +33,85 @@ export interface IMovieExternalAPI {
   facebook_id: string;
   instagram_id: string;
   twitter_id: string;
+}
+*/
+
+export interface IGenre {
+  id: number;
+  name: string;
+}
+
+export interface IProductionCompany {
+  id: number;
+  logo_path: string | null;
+  name: string;
+  origin_country: string;
+}
+
+export interface IProductionCountry {
+  iso_3166_1: string;
+  name: string;
+}
+
+export interface ISpokenLanguage {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+}
+
+export interface ICollection {
+  id: number;
+  name: string;
+  poster_path: string | null;
+  backdrop_path: string | null;
+}
+
+
+export interface IMovieDetailAPI {
+  adult: boolean;
+  backdrop_path: string | null;
+  belongs_to_collection: ICollection | null;
+  budget: number;
+  genres: IGenre[];
+  homepage: string;
+  id: number;
+  imdb_id: string | null;
+  origin_country: string[];
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  production_companies: IProductionCompany[];
+  production_countries: IProductionCountry[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: ISpokenLanguage[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface IMovieCombinationDetail extends IMovie, IMovieDetailAPI{}
+
+export interface MovieFilters {
+  show_filters: string[];
+  // Add other filter properties as needed
+}
+
+
+export interface IKeywordsAPI {
+  page: number;
+  results: IKeyword[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface IKeyword {
+  id: number;
+  name: string;
 }
