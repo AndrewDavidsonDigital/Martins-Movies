428013bc3fb338afbc1e8c917b4e4d5c5d8a6c7c
Andrew Davidson <andrew.davidson.digital@gmail.com>
2025-08-24 14:26:57 +1000

    UI: Add icons for listing page

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,6 +1,8 @@
 export { MenuIcon } from "./MenuIcon";
 export { SearchIcon } from "./SearchIcon";
 export { GlobeIcon } from "./GlobeIcon";
+export { GridIcon } from "./GridIcon";
+export { ListIcon } from "./ListIcon";
 
 export { FacebookIcon } from "./FacebookIcon";
 export { InstagramIcon } from "./InstagramIcon";
