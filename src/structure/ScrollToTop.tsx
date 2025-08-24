28ee802ac261795979b86d50671f179c7a003340
Andrew Davidson <andrew.davidson.digital@gmail.com>
2025-08-24 21:49:53 +1000

    UI: Fix scroll to top visualabilty

--- src/structure/ScrollToTop.tsx
+++ src/structure/ScrollToTop.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { ChevronIcon } from '@/components/icons';
 import { IBaseProps } from '@/utils/interfaces';
 import React, { useState } from 'react';
 
@@ -9,7 +10,7 @@ interface IScrollToTopProps extends IBaseProps {
 
 export default function ScrollToTop(props: IScrollToTopProps) {
 
-  const [shouldHide, _setShouldHide] = useState<boolean>(true);
+  const [shouldHide, _setShouldHide] = useState<boolean>(false);
   
   const scrollToTop = () => {
     document.getElementById(props.targetId)?.scrollIntoView({ behavior: "smooth" });
@@ -18,16 +19,21 @@ export default function ScrollToTop(props: IScrollToTopProps) {
   return (
     <button 
       className={`
-        fixed rounded-full border 
-        border-fuchsia-500 bg-violet-700 
+        fixed rounded-full 
+        z-10
+        !bg-brand 
         aspect-square w-10 
         bottom-6 right-6
         transition-all duration-500
+        drop-shadow-md drop-shadow-brand
+        grid items-center place-content-center
+        -rotate-90
         ${shouldHide ? 'opacity-0' : ''}
       `}
       onClick={scrollToTop}
       aria-label="Scroll to top"
     >
+      <ChevronIcon />
     </button>
   );
 }
