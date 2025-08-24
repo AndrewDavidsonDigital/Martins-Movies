995d27b599c59fbb283c3a967ff7a6708f3e1713
Andrew Davidson <andrew.davidson.digital@gmail.com>
2025-08-24 21:22:10 +1000

    DX: Fix eslint config, add exclusion of `.md` files for dev watching

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 import tseslint from 'typescript-eslint';
+import js from '@eslint/js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,29 +11,35 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
 export default tseslint.config([
-  globalIgnores(['dist']),
   {
+    ignores: ['dist'],
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommendedTypeChecked,
       ...tseslint.configs.stylisticTypeChecked,
-      reactRefresh.configs.vite,
-      reactX.configs['recommended-typescript'],
-      reactDom.configs.recommended,
     ],
     languageOptions: {
-      ...jsxA11y.flatConfigs.recommended.languageOptions,
       ecmaVersion: 2022,
       parserOptions: {
         project: ['./tsconfig.json'],
-        tsconfigRootDir: import.meta.dirname,
+        tsconfigRootDir: __dirname,
       },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { 
+        'argsIgnorePattern': '^_', 
+        'varsIgnorePattern': '^_',
+        'caughtErrorsIgnorePattern': '^_',
+        'destructuredArrayIgnorePattern': '^_'
+      }],
+    },
+  },
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      '@next/next/no-img-element': 'off',
     },
   },
 ]);
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { dev, isServer }) => {
+    if (dev && !isServer) {
+      // Exclude markdown files from triggering reloads in development
+      config.watchOptions = {
+        ...config.watchOptions,
+        ignored: /\.md$/,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
