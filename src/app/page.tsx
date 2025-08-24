f528b416b48e7548076a72f613bff78598400bb4
Andrew Davidson <andrew.davidson.digital@gmail.com>
2025-08-24 16:37:04 +1000

    UI: Add Some copy to the landing page, in case anyone ends up there

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import PageWrapper from "@/structure/PageWrapper";
-import { HeroBanner, LinkElement } from "@/components";
+import { HeroBanner } from "@/components";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <PageWrapper>
       <HeroBanner/>
-      <div className="mx-auto">TBD - Landing</div>
+      <div className="mx-auto min-h-[60dvh] max-w-content grid items-center px-5">
+        <p>Landing page spacer as the page task indicates that the page to build will be within a larger system by the design. <br/> As such the constructed page and functionality is located at <Link href="/movies" className="text-brand hover:brightness-125">/movies</Link></p>
+      </div>
     </PageWrapper>
   );
 }
