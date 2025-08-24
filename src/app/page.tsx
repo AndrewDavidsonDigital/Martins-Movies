02587015c126fbc4becde3fdc0c11b164324d0c2
Andrew Davidson <andrew.davidson.digital@gmail.com>
2025-08-24 18:19:11 +1000

    UI: Fix copy on landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <PageWrapper>
       <HeroBanner/>
       <div className="mx-auto min-h-[60dvh] max-w-content grid items-center px-5">
-        <p>Landing page spacer as the page task indicates that the page to build will be within a larger system by the design. <br/> As such the constructed page and functionality is located at <Link href="/movies" className="text-brand hover:brightness-125">/movies</Link></p>
+        <p>Landing page spacer as the task indicates that the page to build will be within a larger system by the design. <br/> As such the constructed page and functionality is located at <Link href="/movies" className="text-brand hover:brightness-125">/movies</Link></p>
       </div>
     </PageWrapper>
   );
