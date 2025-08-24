32e0452d2167d13a00675bee278271f67d0cea45
Andrew Davidson <andrew.davidson.digital@gmail.com>
2025-08-24 23:47:46 +1000

    DX: More Icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -5,6 +5,8 @@ export { GridIcon } from "./GridIcon";
 export { ListIcon } from "./ListIcon";
 export { ChevronIcon } from "./ChevronIcon";
 export { UserIcon } from "./UserIcon";
+export { HeartIcon } from "./HeartIcon";
+export { ShareIcon } from "./ShareIcon";
 
 export { FacebookIcon } from "./FacebookIcon";
 export { InstagramIcon } from "./InstagramIcon";
